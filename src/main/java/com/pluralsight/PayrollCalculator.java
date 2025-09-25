4c7d0cbf93aa4c66b039b92ec7056ef543350b52
Brian Brooks Jr. <brianbrooksjr1@gmail.com>
2025-09-25 13:21:07 -0500

    Initial PayrollCalculator.java

---
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner myscanner = new Scanner(System.in);
+
+        //Prompt for name
+        System.out.print("Enter your name: ");
+        String name = myscanner.nextLine();
+
+        // Prompt for hours worked
+        System.out.print("Enter hours worked: ");
+        double hoursWorked = myscanner.nextDouble();
+
+        // Prompt for pay rate
+        System.out.print("Enter pay rate: ");
+        double payRate = myscanner.nextDouble();
+
+        //Formula for gross pay is rate mulitiplied by hours worked.
+        double grossPay = hoursWorked * payRate;
+
+        // Display the Employee name and Gross pay
+        System.out.printf("Employee Name: %s\n", name);
+        System.out.printf("Gross Pay: $%.2f\n", grossPay);
+
+        // Close the scanner
+        myscanner.close();
+    }
+}
